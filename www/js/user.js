d0968358dc36676d4bfd6fa405795d1bc5918ef1
Liu, An-Chi <b04209032@ntu.edu.tw>
2016-08-23 17:18:38 +0800

    user basic

--- www/js/user.js
+++ www/js/user.js
@@ -4,6 +4,15 @@
 function moreTempStuff() {
   var tempInfo = document.getElementById("user-form");
   var username = tempInfo.elements[0].value;
-  var password = tempInfo.elements[1].value;
-  alert("username: " + username + "\npassword: " + password);
+  var email = tempInfo.elements[1].value;
+  console.log("username: " + username + "\nemail: " + email);
+  $("#login").hide();
+  $("#info").show();
+  $("#logout").show();
+  $("#info").html('<h1>Hello!</h1><h2>'+username+'</h2><h3>'+email+'</h3>');
+}
+function logOut() {
+  $("#login").show();
+  $("#info").hide();
+  $("#logout").hide();
 }
